e78530149d8c66c156297a6438bae22e13d34e38
ngfgrant <ngfgrant@users.noreply.github.com>
2020-02-27 13:17:00 +0000

    deploy: 1c9f391e27d693e2803822a21233713943324439

---
+++ component---src-pages-index-js-2f1ff5a74788e124c67a.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{RXBc:function(e,t,n){"use strict";n.r(t);var a=n("q1tI"),l=n.n(a),o=n("Wbzz"),r=n("Bl7J"),c=n("vrFN");t.default=function(){return l.a.createElement(r.a,null,l.a.createElement(c.a,{title:"Home"}),l.a.createElement("h1",null,"Hi people"),l.a.createElement("p",null,"Welcome to your new Gatsby site."),l.a.createElement("p",null,"Now go build something great."),l.a.createElement(o.Link,{to:"/page-2/"},"Go to page 2"))}}}]);
+//# sourceMappingURL=component---src-pages-index-js-2f1ff5a74788e124c67a.js.map
