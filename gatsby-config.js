b84d034f4f1b87aae5c81f44a03bd4a1c5fa941d
Niall Grant <ngfgrant@gmail.com>
2020-02-27 11:26:11 +0000

    Add github pages config

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `NOCTURN`,
+    description: `Neurooncology Clinical Trials UK Research Network`,
+    author: `@ngfgrant`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -31,4 +31,5 @@ module.exports = {
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
   ],
+  pathPrefix: "nocturn",
 }
